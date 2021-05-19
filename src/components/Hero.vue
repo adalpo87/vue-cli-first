8ee369cdfca98bf373f38335740285fe9827ed28
adalpo87 <adalpo87@gmail.com>
2021-05-19 16:28:19 +0200

    create le pagine title e hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="jumbotron">
+      <p>Lorem ipsum dolor sit amet consectetur,<br> 
+      adipisicing elit. Perferendis nesciunt repellendus culpa, dolores impedit sed tempora aut,<br>
+       esse ducimus, architecto iste? Nam commodi officia voluptate nostrum vel quos laudantium iure.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style>
+    .jumbotron{
+        color:red;
+    }    
+    
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>La Mia Prima App</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Title'
+}
+</script>
+
+<style>
+
+</style>
